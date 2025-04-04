9f60bb4912068183eaaf1df22a6678eb3fa001e8
SweetKaktus <nathan.pintoaguila.gamedesign@gmail.com>
2025-04-04 06:57:43 +0200

    Implementation en cours

--- app.py
+++ app.py
@@ -1,1 +1,80 @@
 ''' Ce fichier gère le fonctionnement de l'app '''
+import json
+from pathlib import Path
+from liste import Liste
+
+CUR_DIR = Path().parent.resolve()
+DATA_FILE = CUR_DIR / "data" / "lists.json"
+
+
+lists_registered = []
+lists_to_save_in_json = {}
+
+def get_lists_registered():
+    with open(DATA_FILE, 'r') as f:
+        lists_to_save_in_json = json.load(f)
+    if not lists_to_save_in_json:
+        print('Pas de liste enregistrée')
+    else:
+        for key, value in lists_to_save_in_json.items():
+            lists_registered.append(Liste(name=key, tasks_to_do=value['TO DO'], tasks_done=value['DONE']))
+
+def save_lists():
+    for element in lists_registered:
+        lists_to_save_in_json.update({element.name : {'TO DO' : element.tasks_to_do, 'DONE' : element.tasks_done}})
+        print(lists_to_save_in_json)
+    with open(DATA_FILE, 'a') as f:
+        json.dump(lists_to_save_in_json, f, indent=4)
+
+def create_liste(name):
+    lists_registered.append(Liste(name))
+
+def select_screen(lists_registered):
+    print('Lists:')
+    for index, element in enumerate(lists_registered):
+        print(f'{index}. {element}\r')
+
+    print('Choisissez une action:\r')
+    print('1. Selectionner une liste\r2. Creer une liste\r3. Supprimer une liste')
+    manage_input_list_selection()
+
+def manage_input_list_selection():
+    choix = input('Action: ')
+    match choix:
+        case '1':
+            print('Selectionner liste')
+        case '2':
+            list_name = input('Choisissez un nom pour votre liste')
+            create_liste(list_name)
+            save_lists()
+            print_app_title()
+            select_screen(lists_registered)
+        case '3':
+            print('Supprimer liste')
+        case _:
+            print("Votre choix est incorrect, veuillez recommencer")
+            select_screen(lists_registered)
+
+def print_app_title():
+    print('=' * 11)
+    print('TO DO')
+    print('=' * 11)
+
+def select_list():
+    pass
+
+
+if __name__ == '__main__':
+    create_liste(name='Taches du jour')
+    create_liste(name='Taches pro')
+    lists_registered[0].tasks_to_do.append('Manger un croissant')
+    lists_registered[0].tasks_to_do.append('Passer le balai')
+    lists_registered[0].tasks_done.append('Acheter des yaourts')
+    lists_registered[0].tasks_done.append('Faire la vaisselle')
+    lists_registered[1].tasks_to_do.append('traiter alertes')
+    lists_registered[1].tasks_to_do.append('traiter tickets')
+    lists_registered[1].tasks_done.append('comm client')
+    lists_registered[1].tasks_done.append('factu')
+    save_lists()
+    print_app_title()
+    select_screen(lists_registered)
--- liste.py
+++ liste.py
@@ -1,16 +1,86 @@
 ''' Ce fichier sert à créer la classe Liste '''
+import json
+import logging
+from pathlib import Path
+
+CUR_DIR = Path().parent.resolve()
+DATA_FILE = CUR_DIR / "data" / "lists.json"
+LOG_FILE = CUR_DIR / "data" / "infos.log"
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename=LOG_FILE,
+                    filemode="a",
+                    encoding="UTF-8")
 
 # Je pense que je n'ai pas besoin de classe "item" et qu'il suffit que j'ajoute des str dans mes listes
 class Liste:
-    def __init__(self, name):
+    def __init__(self, name, tasks_to_do=[], tasks_done=[]):
         self.name = name
-        self.checked_item_list = [str]
-        self.unchecked_item_list = [str]
+        self.tasks_to_do = []
+        self.tasks_done = []
 
-    def create_liste(self):
-        ''' Cette fonction doit servir à créer une liste et à la stocker
-        dans le json /data/lists.json. Elle doit contenir une liste d'item check, et une liste d'item unchecked '''
-        pass
+    def __str__(self):
+        return self.name
 
-    def create_item(self):
-        pass
+
+
+    # def _get_list(self):
+    #     with open(DATA_FILE, 'r') as f:
+    #         return json.load(f)
+    #
+    # def save_list(self):
+    #     current_lists_saved = self._get_list()
+    #     list_to_save = {self.name : {'Undone' : self.unchecked_item_list, "Done" : self.checked_item_list}}
+    #     current_lists_saved.append(list_to_save)
+    #     with open(DATA_FILE, 'w') as f:
+    #         json.dump(current_lists_saved, f, indent=4)
+    #
+    # def delete_list(self):
+    #     current_lists_saved = self._get_list()
+    #     # lists = {}
+    #     # with open(DATA_FILE, 'r') as f:
+    #     #     lists = json.load(f)
+    #     current_lists_saved.pop(self.name)
+    #     with open(DATA_FILE, 'w') as f:
+    #         json.dump(current_lists_saved, f, indent=4)
+    #
+    #
+    # # def select_list(self):
+    # #     pass
+    # #
+    # # def display_list(self):
+    # #     pass
+    #
+    # def add_item_to_list(self):
+    #     pass
+    #
+    # def check_item(self):
+    #     pass
+    #
+    # def uncheck_item(self):
+    #     pass
+    #
+    # def delete_item(self):
+    #     pass
+
+if __name__ == '__main__':
+    test1 = {}
+    test2 = {}
+    l1 = Liste("test")
+    l1.checked_item_list.append("J'ai fini cette tâche")
+    l1.unchecked_item_list.append("Je dois faire cette tâche")
+    l1.save_list()
+    with open(DATA_FILE, 'r') as f:
+        test1 = json.load(f)
+    l2 = Liste("test2")
+    l2.checked_item_list.append("J'ai fini cette tâche2")
+    l2.unchecked_item_list.append("Je dois faire cette tâche2")
+    l2.save_list()
+    with open(DATA_FILE, 'r') as f:
+        test1 = json.load(f)
+    print(test1)
+    l1.delete_list()
+    with open(DATA_FILE, 'r') as f:
+        test2 = json.load(f)
+    print(test2)
